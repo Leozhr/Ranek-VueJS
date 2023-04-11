8e56cce760f9f4863be93666afe835d4ace2025b
Leozhr <leonardo.leal202@gmail.com>
2023-04-11 09:47:01 -0300

    Layouts & Services

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <main id="main">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+@import url("@/public/css/default.css");
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
+</style>
---
+++ services/axios.ts
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+export const $axios = axios.create({ baseURL: "http://localhost:4000/" })
+
